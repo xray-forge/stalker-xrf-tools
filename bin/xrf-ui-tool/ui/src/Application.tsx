31fab341e10181a73e51d8cfc004e08e12de0274
Neloreck <Neloreck@gmail.com>
2024-03-02 05:42:56 +0200

    Integrate material UI lib.

--- bin/xrf-ui-tool/ui/src/Application.tsx
+++ bin/xrf-ui-tool/ui/src/Application.tsx
@@ -1,50 +1,39 @@
+import { Button, Grid, Input } from "@mui/material";
 import { invoke } from "@tauri-apps/api/tauri";
 import { useState } from "react";
 
-import reactLogo from "@/assets/react.svg";
-import "@/App.css";
-
-function Application() {
-  const [greetMsg, setGreetMsg] = useState("");
+export function Application() {
+  const [greetMessage, setGreetMessage] = useState("");
   const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div className={"container"}>
+    <Grid
+      justifyContent={"center"}
+      alignItems={"center"}
+      direction={"column"}
+      container={true}
+      sx={{ width: "100%", height: "100%" }}
+    >
       <h1>Welcome to Tauri!</h1>
-
-      <div className={"row"}>
-        <a href={"https://vitejs.dev"} target={"_blank"} rel={"noreferrer"}>
-          <img src={"/vite.svg"} className={"logo vite"} alt={"Vite logo"} />
-        </a>
-        <a href={"https://tauri.app"} target={"_blank"} rel={"noreferrer"}>
-          <img src={"/tauri.svg"} className={"logo tauri"} alt={"Tauri logo"} />
-        </a>
-        <a href={"https://reactjs.org"} target={"_blank"} rel={"noreferrer"}>
-          <img src={reactLogo} className={"logo react"} alt={"React logo"} />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
         className={"row"}
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
+        onSubmit={async (event) => {
+          event.preventDefault();
+          setGreetMessage(await invoke("greet", { name }));
         }}
       >
-        <input id={"greet-input"} onChange={(e) => setName(e.currentTarget.value)} placeholder={"Enter a name..."} />
-        <button type={"submit"}>Greet</button>
+        <Input
+          id={"greet-input"}
+          onChange={(event) => setName(event.currentTarget.value)}
+          placeholder={"Enter a name..."}
+        />
+        <Button type={"submit"} variant={"contained"} sx={{ m: "0 16px" }}>
+          Greet
+        </Button>
       </form>
 
-      <p>{greetMsg}</p>
-    </div>
+      <p>{greetMessage}</p>
+    </Grid>
   );
 }
-
-export default Application;
--- bin/xrf-ui-tool/ui/src/main.tsx
+++ bin/xrf-ui-tool/ui/src/main.tsx
@@ -1,11 +1,17 @@
+import { default as CssBaseline } from "@mui/material/CssBaseline";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import Application from "@/Application";
-import "@/styles.css";
+import { Application } from "@/Application";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@/main.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
+    <CssBaseline />
     <Application />
   </StrictMode>
 );
