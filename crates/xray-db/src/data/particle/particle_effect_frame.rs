e67b3211bd068682eaa46801539a7f9a53ce85de
Neloreck <Neloreck@gmail.com>
2024-12-30 04:58:25 +0200

    Added write operations and tests for particle frames/sprites.
    Signed-off-by: Neloreck <Neloreck@gmail.com>

--- crates/xray-db/src/data/particle/particle_effect_frame.rs
+++ crates/xray-db/src/data/particle/particle_effect_frame.rs
@@ -1,9 +1,10 @@
 use crate::chunk::reader::ChunkReader;
-use byteorder::{ByteOrder, ReadBytesExt};
+use crate::chunk::writer::ChunkWriter;
+use byteorder::{ByteOrder, ReadBytesExt, WriteBytesExt};
 use serde::{Deserialize, Serialize};
 use std::io;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ParticleEffectFrame {
   texture_size: (f32, f32),
@@ -14,7 +15,7 @@ pub struct ParticleEffectFrame {
 }
 
 impl ParticleEffectFrame {
-  /// Read particle effect frame data from chunk redder.
+  /// Read frame data from chunk redder.
   pub fn read<T: ByteOrder>(reader: &mut ChunkReader) -> io::Result<ParticleEffectFrame> {
     let particle_frame: ParticleEffectFrame = ParticleEffectFrame {
       texture_size: (reader.read_f32::<T>()?, reader.read_f32::<T>()?),
@@ -27,5 +28,107 @@ impl ParticleEffectFrame {
     assert!(reader.is_ended(), "Expect particle frame chunk to be ended");
 
     Ok(particle_frame)
+  }
+
+  /// Write frame data into the writer.
+  pub fn write<T: ByteOrder>(&self, writer: &mut ChunkWriter) -> io::Result<()> {
+    writer.write_f32::<T>(self.texture_size.0)?;
+    writer.write_f32::<T>(self.texture_size.1)?;
+    writer.write_f32::<T>(self.reserved.0)?;
+    writer.write_f32::<T>(self.reserved.1)?;
+    writer.write_u32::<T>(self.frame_dimension_x)?;
+    writer.write_u32::<T>(self.frame_count)?;
+    writer.write_f32::<T>(self.frame_speed)?;
+
+    Ok(())
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::chunk::reader::ChunkReader;
+  use crate::chunk::writer::ChunkWriter;
+  use crate::data::particle::particle_effect_frame::ParticleEffectFrame;
+  use crate::types::SpawnByteOrder;
+  use fileslice::FileSlice;
+  use serde_json::json;
+  use std::fs::File;
+  use std::io;
+  use std::io::{Seek, SeekFrom, Write};
+  use xray_test_utils::file::read_file_as_string;
+  use xray_test_utils::utils::{
+    get_relative_test_sample_file_path, open_test_resource_as_slice,
+    overwrite_test_relative_resource_as_file,
+  };
+
+  #[test]
+  fn test_read_write_sprite() -> io::Result<()> {
+    let filename: String = String::from("particle_effect_frame.chunk");
+    let mut writer: ChunkWriter = ChunkWriter::new();
+
+    let frame: ParticleEffectFrame = ParticleEffectFrame {
+      texture_size: (15.0, 54.5),
+      reserved: (25.5, 325.5),
+      frame_dimension_x: 155,
+      frame_count: 30,
+      frame_speed: 2857.0,
+    };
+
+    frame.write::<SpawnByteOrder>(&mut writer)?;
+
+    assert_eq!(writer.bytes_written(), 28);
+
+    let bytes_written: usize = writer.flush_chunk_into_file::<SpawnByteOrder>(
+      &mut overwrite_test_relative_resource_as_file(&get_relative_test_sample_file_path(
+        file!(),
+        &filename,
+      ))?,
+      0,
+    )?;
+
+    assert_eq!(bytes_written, 28);
+
+    let file: FileSlice =
+      open_test_resource_as_slice(&get_relative_test_sample_file_path(file!(), &filename))?;
+
+    assert_eq!(file.bytes_remaining(), 28 + 8);
+
+    let mut reader: ChunkReader = ChunkReader::from_slice(file)?
+      .read_child_by_index(0)
+      .expect("0 index chunk to exist");
+
+    let read_frame: ParticleEffectFrame = ParticleEffectFrame::read::<SpawnByteOrder>(&mut reader)?;
+
+    assert_eq!(read_frame, frame);
+
+    Ok(())
+  }
+
+  #[test]
+  fn test_serialize_deserialize_object() -> io::Result<()> {
+    let sprite: ParticleEffectFrame = ParticleEffectFrame {
+      texture_size: (74.0, 236.5),
+      reserved: (263.5, 5369.5),
+      frame_dimension_x: 7352,
+      frame_count: 44,
+      frame_speed: 1.5,
+    };
+
+    let mut file: File = overwrite_test_relative_resource_as_file(
+      &get_relative_test_sample_file_path(file!(), "serialized.json"),
+    )?;
+
+    file.write_all(json!(sprite).to_string().as_bytes())?;
+    file.seek(SeekFrom::Start(0))?;
+
+    let serialized: String = read_file_as_string(&mut file)?;
+
+    assert_eq!(serialized.to_string(), serialized);
+    assert_eq!(
+      sprite,
+      serde_json::from_str::<ParticleEffectFrame>(&serialized)?
+    );
+
+    Ok(())
   }
 }
--- crates/xray-db/src/data/particle/particle_effect_sprite.rs
+++ crates/xray-db/src/data/particle/particle_effect_sprite.rs
@@ -1,9 +1,10 @@
 use crate::chunk::reader::ChunkReader;
+use crate::chunk::writer::ChunkWriter;
 use byteorder::ByteOrder;
 use serde::{Deserialize, Serialize};
 use std::io;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ParticleEffectSprite {
   shader_name: String,
@@ -24,5 +25,97 @@ impl ParticleEffectSprite {
     );
 
     Ok(particle_sprite)
+  }
+
+  /// Write sprite data into the writer.
+  pub fn write<T: ByteOrder>(&self, writer: &mut ChunkWriter) -> io::Result<()> {
+    writer.write_null_terminated_win_string(&self.shader_name)?;
+    writer.write_null_terminated_win_string(&self.texture_name)?;
+
+    Ok(())
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::chunk::reader::ChunkReader;
+  use crate::chunk::writer::ChunkWriter;
+  use crate::data::particle::particle_effect_sprite::ParticleEffectSprite;
+  use crate::types::SpawnByteOrder;
+  use fileslice::FileSlice;
+  use serde_json::json;
+  use std::fs::File;
+  use std::io;
+  use std::io::{Seek, SeekFrom, Write};
+  use xray_test_utils::file::read_file_as_string;
+  use xray_test_utils::utils::{
+    get_relative_test_sample_file_path, open_test_resource_as_slice,
+    overwrite_test_relative_resource_as_file,
+  };
+
+  #[test]
+  fn test_read_write_sprite() -> io::Result<()> {
+    let filename: String = String::from("particle_effect_sprite.chunk");
+    let mut writer: ChunkWriter = ChunkWriter::new();
+
+    let sprite: ParticleEffectSprite = ParticleEffectSprite {
+      shader_name: String::from("shader_name"),
+      texture_name: String::from("texture_name"),
+    };
+
+    sprite.write::<SpawnByteOrder>(&mut writer)?;
+
+    assert_eq!(writer.bytes_written(), 25);
+
+    let bytes_written: usize = writer.flush_chunk_into_file::<SpawnByteOrder>(
+      &mut overwrite_test_relative_resource_as_file(&get_relative_test_sample_file_path(
+        file!(),
+        &filename,
+      ))?,
+      0,
+    )?;
+
+    assert_eq!(bytes_written, 25);
+
+    let file: FileSlice =
+      open_test_resource_as_slice(&get_relative_test_sample_file_path(file!(), &filename))?;
+
+    assert_eq!(file.bytes_remaining(), 25 + 8);
+
+    let mut reader: ChunkReader = ChunkReader::from_slice(file)?
+      .read_child_by_index(0)
+      .expect("0 index chunk to exist");
+
+    let read_sprite: ParticleEffectSprite =
+      ParticleEffectSprite::read::<SpawnByteOrder>(&mut reader)?;
+
+    assert_eq!(read_sprite, sprite);
+
+    Ok(())
+  }
+
+  #[test]
+  fn test_serialize_deserialize_object() -> io::Result<()> {
+    let sprite: ParticleEffectSprite = ParticleEffectSprite {
+      shader_name: String::from("shader_name"),
+      texture_name: String::from("texture_name"),
+    };
+
+    let mut file: File = overwrite_test_relative_resource_as_file(
+      &get_relative_test_sample_file_path(file!(), "serialized.json"),
+    )?;
+
+    file.write_all(json!(sprite).to_string().as_bytes())?;
+    file.seek(SeekFrom::Start(0))?;
+
+    let serialized: String = read_file_as_string(&mut file)?;
+
+    assert_eq!(serialized.to_string(), serialized);
+    assert_eq!(
+      sprite,
+      serde_json::from_str::<ParticleEffectSprite>(&serialized)?
+    );
+
+    Ok(())
   }
 }
