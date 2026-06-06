78f5d9321441d8ef69ed99afcf9a5d253b5665d0
Neloreck <Neloreck@gmail.com>
2026-06-06 04:41:57 +0300

    Self-host Roboto fonts and remove `@fontsource/roboto`. Update project files to reference local font assets.

--- bin/xrf-ui/src/main.tsx
+++ bin/xrf-ui/src/main.tsx
@@ -1,10 +1,5 @@
 import "reflect-metadata";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
