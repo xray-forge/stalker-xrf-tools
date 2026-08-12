db6b5280237bf8eccede12f145cd79fddb5eac5e
Neloreck <Neloreck@gmail.com>
2026-08-12 23:45:57 +0300

    Move dependency injection setup from `Application` `ApplicationProvider` to streamline container configuration and improve code modularity.

--- bin/xrf-ui/src/applications/Application.tsx
+++ bin/xrf-ui/src/applications/Application.tsx
@@ -1,30 +1,15 @@
-import { ContainerConfig, EventsPlugin } from "@wirestate/core";
-import { ContainerProvider } from "@wirestate/react";
-import { ReactElement, useMemo } from "react";
+import { ReactElement } from "react";
 
 import { ApplicationProvider } from "@/applications/ApplicationProvider";
 import { ApplicationRouter } from "@/applications/ApplicationRouter";
-import { ErrorCaptureService, NotificationsService } from "@/core/store/notifications";
-import { ProjectService } from "@/core/store/project";
-import { SettingsService } from "@/core/store/settings";
 
 /**
  * The root container.
  */
 export function Application(): ReactElement {
-  const config: ContainerConfig = useMemo(
-    () => ({
-      bindings: [ProjectService, SettingsService, NotificationsService, ErrorCaptureService],
-      plugins: [new EventsPlugin()],
-    }),
-    []
-  );
-
   return (
-    <ContainerProvider config={config}>
-      <ApplicationProvider>
-        <ApplicationRouter />
-      </ApplicationProvider>
-    </ContainerProvider>
+    <ApplicationProvider>
+      <ApplicationRouter />
+    </ApplicationProvider>
   );
 }
--- bin/xrf-ui/src/applications/ApplicationProvider.tsx
+++ bin/xrf-ui/src/applications/ApplicationProvider.tsx
@@ -1,8 +1,13 @@
 import { GlobalStyles } from "@mui/material";
 import { default as CssBaseline } from "@mui/material/CssBaseline";
 import { Theme, ThemeProvider } from "@mui/material/styles";
+import { ContainerConfig, EventsPlugin } from "@wirestate/core";
+import { ContainerProvider } from "@wirestate/react";
 import { ReactNode, useMemo } from "react";
 
+import { ErrorCaptureService, NotificationsService } from "@/core/store/notifications";
+import { ProjectService } from "@/core/store/project";
+import { SettingsService } from "@/core/store/settings";
 import { createApplicationTheme } from "@/lib/theme";
 
 interface IApplicationProviderProps {
@@ -12,26 +17,38 @@ interface IApplicationProviderProps {
 export function ApplicationProvider({ children }: IApplicationProviderProps) {
   const theme: Theme = useMemo(() => createApplicationTheme(), []);
 
+  const config: ContainerConfig = useMemo(
+    () => ({
+      bindings: [ProjectService, SettingsService, NotificationsService, ErrorCaptureService],
+      plugins: [new EventsPlugin()],
+    }),
+    []
+  );
+
   return (
-    <ThemeProvider
-      defaultMode={"dark"}
-      disableTransitionOnChange={true}
-      modeStorageKey={"theme"}
-      noSsr={true}
-      theme={theme}
-    >
-      <CssBaseline enableColorScheme={true} />
-      <GlobalStyles
-        styles={{
-          "html, body, #root": {
-            width: "100%",
-            height: "100%",
-            minHeight: 360,
-            minWidth: 400,
-          },
-        }}
-      />
-      {children}
-    </ThemeProvider>
+    <ContainerProvider config={config}>
+      <ThemeProvider
+        defaultMode={"dark"}
+        disableTransitionOnChange={true}
+        modeStorageKey={"theme"}
+        noSsr={true}
+        theme={theme}
+      >
+        <CssBaseline enableColorScheme={true} />
+
+        <GlobalStyles
+          styles={{
+            "html, body, #root": {
+              width: "100%",
+              height: "100%",
+              minHeight: 360,
+              minWidth: 400,
+            },
+          }}
+        />
+
+        {children}
+      </ThemeProvider>
+    </ContainerProvider>
   );
 }
