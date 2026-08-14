a101dc3de9d326c610053198fe9bde7931102632
Neloreck <Neloreck@gmail.com>
2026-08-15 01:31:31 +0300

    Adjust ApplicationLauncher layout: reduce maxWidth to 1280 for improved responsiveness.

--- bin/xrf-ui/src/core/launcher/ApplicationLauncher.tsx
+++ bin/xrf-ui/src/core/launcher/ApplicationLauncher.tsx
@@ -141,7 +141,7 @@ export function ApplicationLauncher({ applications, groups }: IApplicationLaunch
   return (
     <EditorLayout toolbar={<EditorToolbar />}>
       <Box sx={{ width: "100%", height: "100%", overflowY: "auto", padding: 3 }}>
-        <Box sx={{ width: "100%", maxWidth: 1600, marginX: "auto" }}>
+        <Box sx={{ width: "100%", maxWidth: 1280 }}>
           <Box sx={{ marginBottom: 2 }}>
             <Typography component={"h1"} variant={"h5"}>
               Applications
